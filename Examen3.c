53206984bca09ad3d6b48a9b32e91d579c503b5e
Laura Castillo <102078623+LauCastle@users.noreply.github.com>
2022-12-12 16:08:19 -0600

    Terminado

--- Examen3.c
+++ Examen3.c
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
 
 struct nodo{
 	struct nodo* hijo_izq;
@@ -11,7 +13,7 @@ struct nodo{
 
 	int info;
 };
-
+//nuevo nodo
 struct nodo* nuevoNodo(int dato){
 	size_t tam_nodo = sizeof(struct nodo);
 
@@ -23,7 +25,7 @@ struct nodo* nuevoNodo(int dato){
 	
 	return nuevo_nodo;
 }
-
+//funcion insertar nodo
 void insertarNodo(struct nodo* nd, int dato){
 	if(dato> nd -> info){
 		if(nd -> hijo_der == NULL){
@@ -39,7 +41,7 @@ void insertarNodo(struct nodo* nd, int dato){
 		}
 	}
 }
-
+//funcion preorden
 void preorden(struct nodo* nd){
 	if(nd != NULL){
 		printf("%d ", nd->info);
@@ -47,10 +49,27 @@ void preorden(struct nodo* nd){
 		preorden(nd->hijo_der);
 	}
 }
-
+//funcion inorden
+void inorden(struct nodo* nd){
+	if(nd != NULL){
+		inorden(nd->hijo_izq);
+		printf("%d ",nd->info);
+		inorden(nd->hijo_der);
+	}
+}
+//funcion postorden
+void postorden(struct nodo* nd){
+	if(nd !=NULL){
+		postorden(nd->hijo_izq);
+		postorden(nd->hijo_der);
+		printf("%d ",nd->info);
+	}
+}
+//funcion buscar
 int lvl=0;
 void buscar(struct nodo* nd, int n){
 	lvl++;
+
 	if(n==nd->info){
 		printf("El nuemro %d esta en el nivel %d\n",n, lvl);
 	}else if(n > nd->info && nd->hijo_der != NULL){
@@ -63,15 +82,16 @@ void buscar(struct nodo* nd, int n){
 }
 
 int main(){
-	//char c;
+	int opc;
+	char repetir = TRUE;
 	int n, r, dato, dt, c;
 	int i=0;
 	struct nodo* raiz= NULL;
-	//para ingresar manualmente los datos del arbol
-	printf("Ingresa la raiz de tu arbol: ");
+
+	//Ingresar valores manualmente
+	printf("Ingresa la raiz del arbol: ");
 	scanf("%d",&r);
 	raiz = nuevoNodo(r);
-
 	do{
 		printf("Ingresa cuantos nodos: ");
 		scanf("%d",&dt);
@@ -81,33 +101,57 @@ int main(){
 			insertarNodo(raiz,dato);
 			i++;
 		}
-		printf("Deseas agregar mas nodos? \n");
-		printf("    1.Si      2.No \n");
-		printf(":");
+		printf("\n¿Deseas agregar mas nodos? S/N\n");
+		printf("     1.- Si     2.-No    \n");
+		printf(": ");
 		scanf("%d",&c);
 		i=0;
-
-		if(c !=1){
-			printf("Arbol terminado\n");
+		
+		if(c!=1){
+			printf("\nArbol Terminado\n");
 			break;
 		}
 	}while(1);
+	
+	//Menú de opciones
+	do{
+		//system("clear"); para borrar, pero al hacerlo se borra el arbol ya agregado anteriormentee
 
+		printf("\n---Opciones para el arbol---\n");
+		printf("1.- Buscar un valor dentro del arbol\n");
+		printf("2.- Ordenar en Pre-orden\n");
+		printf("3.- Ordenar en In-orden\n");
+		printf("4.- Ordenar en Post-orden\n");
+		printf("5.- SALIR\n");
 
+		printf("\nIngrese una opcion: ");
+		scanf("%d",&opc);
 
-	//printf("Ingresa el nodo: ");
-	//scanf("%d", &dato);
-	//insertarNodo(raiz,dato);
-
-	printf("\nArbol en pre-orden: \n");
-	preorden(raiz);
-	printf("\n");
-
-	printf("Ingrese el numero a buscar: ");
-	scanf("%d",&n);
-	buscar(raiz,n);
-
+		switch (opc){
+			case 1:
+				printf("Ingresa el numero a buscar: ");
+				scanf("%d",&n);
+				buscar(raiz,n);
+				break;
+			case 2:
+				printf("\nArbol en pre-orden: ");
+				preorden(raiz);
+				printf("\n");
+				break;
+			case 3:
+				printf("\nArbol en in-orden: ");
+				inorden(raiz);
+				printf("\n");
+				break;
+			case 4: 
+				printf("\nArbol en post-orden: ");
+				postorden(raiz);
+				printf("\n");
+				break;
+			case 5:
+				repetir = FALSE;
+				break;
+		}
+	}while (repetir);
 	return 0;
 }
-
-			
